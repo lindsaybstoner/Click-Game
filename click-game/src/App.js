0e617bb04b08fcbfa90773ac7c185baf7f413138
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-06-18 16:22:23 -0400

    add footer styling

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Main from './components/Main';
-/* import Footer from './components/Footer'; */
+import Footer from './components/Footer';
 import "./style.css";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <Nav />
     <Header />
     <Main />
-    {/* <Footer /> */}
+    <Footer />
     </>
   );
 }
---
+++ click-game/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css";
+
+function Footer () {
+    return (
+       <footer>
+           <div className="footer-text">Clicky Game</div>
+       </footer>
+    )
+}
+
+export default Footer;
