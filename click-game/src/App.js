35e5b83e04314e170aba9e1752bfe3e6dc322f14
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-06-18 11:12:08 -0400

    set up component folders

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -2,22 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>The Click Game</h1>
+    </>
   );
 }
 
