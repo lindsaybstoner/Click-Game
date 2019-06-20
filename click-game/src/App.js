150eaff0e4e75f4515cc5cba203326713b59eea8
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-06-20 14:49:26 -0400

    try to get images to randomize

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -9,43 +9,83 @@ import characters from "./characters.json";
 
 
 class App extends Component {
-  state = {
-    characters,
-    currentScore: 0,
-    highestScore: 0,
-    heading: '',
-  }
+	state = {
+		characters,
+		currentScore: 0,
+		highestScore: 0,
+		heading: '',
+	}
 
-  getId = (id) => {
-	let idsClicked =[];
-	idsClicked.push(id);
-	console.log(idsClicked)
-  };
+	getId = (id) => {
+		let idsClicked = [];
+		idsClicked.push(id);
+		console.log(idsClicked)
 
-  render() {
-    return (
-      <>
-        <Nav
-          currentScore={this.state.currentScore}
-          highestScore={this.state.highestScore}
-          heading={this.state.heading || "Click an image to begin!"}
-        />
-        <Header />
-        <Main>
-          {this.state.characters.map(character => (
-            <ImageCard
-              id={character.id}
-              key={character.id}
-              name={character.name}
-              image={character.image}
-              getId={this.getId}
-            />
-          ))}
-        </Main>
-        <Footer />
-      </>
-    );
-  }
+		this.setState({});
+	};
+
+	// handleIncrement increases this.state.count by 1
+	handleIncrement = () => {
+		// We always use the setState method to update a component's state
+		this.setState({ currentScore: this.state.currentScore + 1 });
+
+		/* if (this.state.currentScore >= this.state.highestScore) {
+			this.setState({ highestScore: this.state.currentScore });
+		} */
+	};
+
+	handleHighestScore = () => {
+		if (this.state.currentScore >= this.state.highestScore) {
+			this.setState({ highestScore: this.state.currentScore });
+		}
+	}
+
+	shuffle = (characters) => {
+		var currentIndex = this.state.characters.length, temporaryValue, randomIndex;
+
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = characters[currentIndex];
+			characters[currentIndex] = characters[randomIndex];
+			characters[randomIndex] = temporaryValue;
+		}
+
+		return this.setState({ characters });
+	}
+
+	render() {
+		return (
+			<>
+				<Nav
+					currentScore={this.state.currentScore}
+					highestScore={this.state.highestScore}
+					heading={this.state.heading || "Click an image to begin!"}
+				/>
+				<Header />
+				<Main>
+					{this.state.characters.map(character => (
+						<ImageCard
+							id={character.id}
+							key={character.id}
+							name={character.name}
+							image={character.image}
+							getId={this.getId}
+							handleIncrement={this.handleIncrement}
+							handleHighestScore={this.handleHighestScore}
+							shuffle={this.shuffle}
+						/>
+					))}
+				</Main>
+				<Footer />
+			</>
+		);
+	}
 }
 
 
--- click-game/src/components/ImageCard/index.js
+++ click-game/src/components/ImageCard/index.js
@@ -4,7 +4,12 @@ import "./style.css";
 const ImageCard = (props) => {
   return (
     <div className="card">
-      <span onClick={() => props.getId(props.id)}>
+      <span onClick={() => {
+        props.handleIncrement(props.currentScore)
+        props.handleHighestScore(props.highestScore)
+        props.getId(props.id)
+        props.shuffle(props.character)
+      }}>
         <img alt={props.name} src={props.image} />
       </span>
     </div>
